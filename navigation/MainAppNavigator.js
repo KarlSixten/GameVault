0c00160a09d7fa304eafd85d4251da20c56a8732
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-06 20:09:16 +0200

    Added icons for bottom nav bar. Added image with color inspiration for design

--- navigation/MainAppNavigator.js
+++ navigation/MainAppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import LibraryScreen from '../screens/Library';
 import WishlistScreen from '../screens/Wishlist';
@@ -9,7 +10,30 @@ const Tab = createBottomTabNavigator();
 
 export default function MainAppNavigator() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Library') {
+                iconName = focused ? 'library' : 'library-outline';
+              } else if (route.name === 'Wishlist') {
+                iconName = focused ? 'gift' : 'gift-outline';
+              } else if (route.name === 'Profile') {
+                iconName = focused ? 'person-circle' : 'person-circle-outline';
+              }
+  
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+            tabBarStyle: {
+              backgroundColor: 'white',
+              paddingBottom: 5,
+              height: 60,
+            },
+          })}
+        >
             <Tab.Screen name="Library" component={LibraryScreen} />
             <Tab.Screen name="Wishlist" component={WishlistScreen} />
             <Tab.Screen name="Profile" component={ProfileScreen} />
