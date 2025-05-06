30cede77688e3e75e90bb678663e413bb46dcde6
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-06 18:18:24 +0200

    Added ProfileScreen to navigation

--- navigation/MainAppNavigator.js
+++ navigation/MainAppNavigator.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LibraryScreen from '../screens/Library';
 import WishlistScreen from '../screens/Wishlist';
+import ProfileScreen from '../screens/Profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,6 +12,7 @@ export default function MainAppNavigator() {
         <Tab.Navigator>
             <Tab.Screen name="Library" component={LibraryScreen} />
             <Tab.Screen name="Wishlist" component={WishlistScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
     );
 }
