bfe47a73240951248986a8dca52c667e6bb58775
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-06 17:20:14 +0200

    Added placeholder library and wishlist screens

---
+++ screens/Library.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function LibraryScreen() {
+    return (
+        <View>
+            <Text>This is the library screen</Text>
+        </View>
+    );
+}
---
+++ screens/Wishlist.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function WishlistScreen() {
+    return (
+        <View>
+            <Text>This is the Wishlist screen</Text>
+        </View>
+    );
+}
