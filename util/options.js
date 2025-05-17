32dd2d1691067337fc33a96f5f6b7c3fa3edf749
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-17 16:01:35 +0200

    Created options for better reusability

---
+++ util/options.js
@@ -0,0 +1,33 @@
+export const platformOptions = [
+    { label: 'Select a Platform...', value: '' },
+    { label: 'PC (Steam, Epic, etc.)', value: 'PC' },
+    { label: 'PlayStation 5 (PS5)', value: 'PS5' },
+    { label: 'Xbox Series X/S', value: 'Xbox Series X/S' },
+    { label: 'Nintendo Switch', value: 'Nintendo Switch' },
+    { label: 'iOS', value: 'iOS' },
+    { label: 'Android', value: 'Android' },
+    { label: 'Other', value: 'Other' },
+];
+
+export const genreOptions = [
+    { label: 'Select a genre...', value: null },
+    { label: 'Action', value: 'Action' },
+    { label: 'Adventure', value: 'Adventure' },
+    { label: 'Role-Playing (RPG)', value: 'RPG' },
+    { label: 'Strategy', value: 'Strategy' },
+    { label: 'Simulation', value: 'Simulation' },
+    { label: 'Sports', value: 'Sports' },
+    { label: 'Shooter', value: 'Shooter' },
+    { label: 'Puzzle', value: 'Puzzle' },
+    { label: 'Other', value: 'Other' },
+];
+
+export const statusOptions = [
+    { label: 'Select a status...', value: null },
+    { label: 'Playing', value: 'Playing' },
+    { label: 'On Hold', value: 'On Hold' },
+    { label: 'Completed', value: 'Completed' },
+    { label: '100% Completed', value: '100%' },
+    { label: 'Dropped', value: 'Dropped' },
+    { label: 'Not Started', value: 'Not Started' },
+];
